0ba7929c2186714a95fb9ca7f3d1bd7e23fb1f6f
aqajade <gholamreza.aqajade@gmail.com>
2024-10-19 20:58:38 +0700

    benerin baris 9 pengguna ts di models

--- lib/models/pengguna.ts
+++ lib/models/pengguna.ts
@@ -1,9 +1,10 @@
-import { Schema,model,models } from "mongoose";
+import { Schema, model, models } from "mongoose";
 
 const PenggunaSchema = new Schema({
-  email: {type:String,required:true,unique:true},
-  username: {type:String,required:true,unique:true},
-  password: {type:String,required:true},
-})
-const Pengguna = models.Pengguna || model("PenggunaSchema")
-export default Pengguna
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+// const Pengguna = models.Pengguna || model("PenggunaSchema")
+const Pengguna = models.Pengguna || model("Pengguna", PenggunaSchema);
+export default Pengguna;
