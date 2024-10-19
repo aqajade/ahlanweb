ddfd82b2ded7e80b9228b4105cdfb07e8e6c03ef
aqajade <gholamreza.aqajade@gmail.com>
2024-10-19 20:29:13 +0700

    fix mongodb konek konek wayaw

--- lib/koneksisimplestmongodb.ts
+++ lib/koneksisimplestmongodb.ts
@@ -1,15 +1,18 @@
-
-
-
-
+import dotenv from "dotenv";
 import mongoose from "mongoose";
+dotenv.config();
 const db = process.env.MONGODB_URL;
 const konekmongodb = async () => {
   try {
-    await mongoose.connect(db)
-    console.log("konek alhamdulillah")
+    if (!db) {
+      throw new Error(
+        "MONGODB_URL is not defined in the environment variables."
+      );
+    }
+    await mongoose.connect(db);
+    console.log("konek alhamdulillah");
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
-export default konekmongodb
+};
+export default konekmongodb;
