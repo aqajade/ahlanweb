8e79085ddc9ce7fb5a161335d7f03d81441c8d1e
aqajade <gholamreza.aqajade@gmail.com>
2024-10-20 07:57:33 +0700

    benerin contoh route sm mongodb get dr collection tset trnyt

--- app/api/contohs/route.ts
+++ app/api/contohs/route.ts
@@ -4,10 +4,12 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   // dibawah ini matiin koneksi sementara
-  // await dbconnect(); //mastiin db aman sblm request
+  await dbconnect(); //mastiin db aman sblm request
   try {
-    const contohs = await Contoh.find({});
-    return NextResponse.json(contohs);
+    // const contohs = await Contoh.find({});
+    // return NextResponse.json(contohs);
+    const contoh = await Contoh.find({});
+    return NextResponse.json(contoh);
   } catch (err: any) {
     return NextResponse.json({ error: err.message });
   }
